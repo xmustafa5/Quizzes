01d6cd222ee84308e9f711a1a71a0069d424a09c
Mustafa mohammed <107437940+xmustafa5@users.noreply.github.com>
2024-02-28 15:45:16 +0300

    handling the componeners cards

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <script setup>
 import q from "./data/quizes.json";
-import { ref ,watch } from "vue";
+import { ref, watch } from "vue";
+import Card from "./components/Card.vue";
 const quizzes = ref(q);
-const search = ref('');
+const search = ref("");
 
-watch(search,()=>{
-  quizzes.value = q.filter(quiz => quiz.name.toLowerCase().includes(search.value.toLowerCase()) )
-})
-
+watch(search, () => {
+  console.log("test");
+  quizzes.value = q.filter((quiz) =>
+    quiz.name.toLowerCase().includes(search.value.toLowerCase())
+  );
+});
 </script>
 
 <template>
@@ -17,13 +20,7 @@ watch(search,()=>{
       <input v-model.term="search" type="text" placeholder="search...." />
     </header>
     <div class="options-container">
-      <div class="card" v-for="quiz in quizzes" :key="quiz.id">
-        <img :src="quiz.image" alt="" />
-        <div class="card-text">
-          <h2>{{ quiz.name }}</h2>
-          <p>{{ quiz.questions.length }} questions</p>
-        </div>
-      </div>
+      <Card v-for="quiz in quizzes" :key="quiz.id" :quiz="quiz" />
     </div>
   </div>
 </template>
@@ -49,26 +46,6 @@ header input {
   background-color: rgba(128, 128, 128, 0.1);
   padding: 10px;
   border-radius: 5px;
-}
-.card {
-  width: 310px;
-  overflow: hidden;
-  border-radius: 2%;
-  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
-  margin-bottom: 35px;
-  margin-right: 20px;
-  cursor: pointer;
-}
-.card img {
-  width: 100%;
-  height: 190px;
-  margin: 0;
-}
-.card .card-text {
-  padding: 0 5px;
-}
-.card .card-text h2 {
-  font-weight: bold;
 }
 
 .options-container {
